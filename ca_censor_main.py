72b7634813277e605ec2b6c1e1e103b4fb306acc
Trenton Wesche <TrWesche@live.com>
2019-11-01 18:13:13 -0500

    V2 of censor function and list handling capable

--- ca_censor_main.py
+++ ca_censor_main.py
@@ -4,21 +4,63 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-def censor_string_val(search_text: str, censor_search_val, censor_character:str = "x"):
-    t_output = []
-    output = ""
-    t_censor_value = []
-    for i in range(len(censor_search_val)):
-        t_censor_value.append(censor_character)
-    # t_censor_value = [t_censor_value.append(censor_character) for i in range(len(censor_search_val))]
-    censor_value = "".join(t_censor_value)
+# V2 of the censor single string has been built to address the false positives of the original function which found the word when it was a part of a different object
+def censor_single_string(search_text: str, censor_search_val, c_character: str = "x"):
+    st_list = search_text.split(" ")                                        # Convert search text into a list of strings
+    len_st_list = len(st_list)
+    cs_val_list = censor_search_val.split(" ")                              # Convert censor search value into a list of strings
+    cs_val_lens = [len(val) for val in cs_val_list]                 # Calculate the length of each censor search value
+    len_cs_val_list = len(cs_val_list)
 
-    replace_start_location = search_text.find(censor_search_val)
-    output = search_text
-    while replace_start_location >= 0:
-        t_output = output.partition(censor_search_val)
-        output = (f"{t_output[0]}{censor_value}{t_output[2]}")
-        replace_start_location = output.find(censor_search_val)
-    return output
+    # Create the censor replacement values using the exact length of the values being used to perform the search
+    censor_values = []
+    for val in cs_val_lens:
+        t_censor_value = []
+        for i in range(val):
+            t_censor_value.append(c_character)
+        censor_values.append("".join(t_censor_value))
 
-print(censor_string_val(email_one,"learning algorithms"))
+    # Begin searching list items for the search term 
+    for i in range(len_st_list):
+        replace_start_location = st_list[i].lower().find(cs_val_list[0])                #Makes the text in the item lowercase and searches the list value for the first search term 
+        if replace_start_location >= 0:                                                 #If something found continue
+
+            if (i + len_cs_val_list - 1) > len_st_list:                                 # Check to exit loop and counter calls outside of list range
+                break
+
+            t_checks = True
+            for i_cs_val in range(len_cs_val_list):
+                if len(st_list[i+i_cs_val]) != cs_val_lens[i_cs_val]:                   # Check to see if the cs search value and the value in the list are the same length
+                    t_output = st_list[i+i_cs_val].partition(cs_val_list[i_cs_val])     # Break the string into 3 parts around the search value
+
+                    if t_checks:
+                        if len(t_output[0]) > 0:                                        #Check the value preceeding the search value
+                            t_checks = not t_output[0][-1].isalpha() and ord(t_output[0][-1]) != 45 #Not alphabetical and not a dash (-)
+                        else:
+                            t_checks = True
+                    if t_checks:
+                        if len(t_output[2]) > 0:                                        #Check the value following the search value
+                            t_checks = (not t_output[2][0].isalpha() or (len(t_output[2]) == 1 and ord(t_output[2][0]) == 115)) and ord(t_output[2][0]) != 45   #Not alphabetical and not a dash (-)
+                        else:
+                            t_checks = True
+
+            if t_checks:
+                for i_cs_val in range(len_cs_val_list):
+                    st_list[i+i_cs_val] = censor_values[i_cs_val]
+
+    return " ".join(st_list)
+
+# print(censor_single_string(email_one,"learning algorithms"))
+
+# print(censor_single_string(email_two,"her"))
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+def censor_list_strings(search_text: str, censor_search_vals: list, censor_character:str = "x"):
+    output_text = search_text
+    for val in censor_search_vals:
+        output_text = censor_single_string(output_text,val,censor_character)
+
+    return output_text
+
+print(censor_list_strings(email_two,proprietary_terms))
--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,3 +3,57 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+# def censor_single_string(search_text: str, censor_search_val, c_character:str = "x"):
+#     t_output = []
+#     output = ""
+#     t_censor_value = []
+#     for i in range(len(censor_search_val)):
+#         t_censor_value.append(c_character)
+#     # t_censor_value = [t_censor_value.append(censor_character) for i in range(len(censor_search_val))]
+#     censor_value = "".join(t_censor_value)
+
+#     replace_start_location = search_text.find(censor_search_val)
+#     output = search_text
+#     while replace_start_location >= 0:
+#         t_output = output.partition(censor_search_val)
+#         output = (f"{t_output[0]}{censor_value}{t_output[2]}")
+#         replace_start_location = output.find(censor_search_val)
+#     return output
+
+# print(censor_single_string(email_one,"learning algorithms"))
+
+
+
+
+def censor_single_string(search_text: str, censor_search_val, c_character:str = "x"):
+    t_output = []
+    output = ""
+    t_censor_value = []
+    for i in range(len(censor_search_val)):
+        t_censor_value.append(c_character)
+    censor_value = "".join(t_censor_value)
+
+    replace_start_location = search_text.find(censor_search_val)
+    output = search_text
+    while replace_start_location >= 0:
+        t_output = output.partition(censor_search_val)
+        # Perform additional checks to make sure the value found is not part of a different word.
+        if len(t_output[0]) > 0:                        #Check the value preceeding the search value
+            t_checks = not t_output[-1][0].isalpha() and ord(t_output[-1][0]) != 45 #Not alphabetical and not a dash (-)
+        else:
+            t_checks = True
+
+        if (len(t_output[2]) > 0) and t_checks:         #Check the value following the search value
+            t_checks = not t_output[2][0].isalpha() and ord(t_output[2][0]) != 45   #Not alphabetical and not a dash (-)
+        else:
+            t_checks = True
+        if t_checks == True:
+            output = (f"{t_output[0]}{censor_value}{t_output[2]}")
+        
+        if replace_start_location + 1 < len(search_text):   #Make sure the next search does not catch a previously found string & will not cause an error
+            find_strt_loc = replace_start_location + 1      
+        else:
+            break
+        replace_start_location = output.find(censor_search_val,find_strt_loc)
+    return output
